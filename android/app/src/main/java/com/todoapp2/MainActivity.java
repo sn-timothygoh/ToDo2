bc41602beedb717f00a63da6a7fd7c76fd842afb
Vin <chanvs-wa16@student.tarc.edu.my>
2020-03-26 22:23:01 +0800

    5th commit

--- android/app/src/main/java/com/todoapp2/MainActivity.java
+++ android/app/src/main/java/com/todoapp2/MainActivity.java
@@ -1,9 +1,18 @@
 package com.todoapp2;
 
+import android.os.Bundle;
+
 import com.facebook.react.ReactActivity;
+
+import org.devio.rn.splashscreen.SplashScreen;
 
 public class MainActivity extends ReactActivity {
 
+   @Override
+   protected  void onCreate(Bundle savedIntanceState){
+     SplashScreen.show(this);
+     super.onCreate(savedIntanceState);
+   }
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
@@ -13,3 +22,4 @@ public class MainActivity extends ReactActivity {
     return "todoApp2";
   }
 }
+//
